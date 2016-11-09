a7cff2a017a8623f4cc4b677a39acea31e9d7f42
Charles Zhang <charleszhang99@yahoo.com>
2016-11-09 02:07:30 -0500

    Unoobify code

--- hal/frc-hal.lisp
+++ hal/frc-hal.lisp
@@ -20,9 +20,38 @@
 (defcfun "setDIO" :void (digital-port-pointer :pointer) (value :int16) (status :pointer :int32))
 (defcfun "getDIO" :bool (digital-port-pointer :pointer) (status :pointer :int32))
 
-(defparameter nullptr (make-pointer 0))
-(defvar *status-code-ptr* (foreign-alloc :pointer))
-(defvar *dio-ports* (make-array 26 :element-type 'sb-sys:system-area-pointer :initial-element nullptr))
+(defvar *status-code-ptr* (foreign-alloc :int32))
+
+(defclass dio-port ()
+  ((%channel :reader dio-port-channel :initarg :channel)
+   (%pointer :reader dio-port-pointer :initarg :pointer)
+   (%direction :accessor dio-port-direction :initarg :direction)))
+
+(defun make-dio-port (channel direction)
+  "Takes a channel and direction, where input is either :OUTPUT or :INPUT."
+  (unless (<= 0 channel 25)
+    (error "Not a port number between 0 and 26."))
+  (make-instance 'dio-port :channel channel
+                           :pointer (let ((pointer (initializedigitalport (getport channel) *status-code-ptr*)))
+                                      (allocatedio (aref *dio-ports* channel)
+                                                   (ecase direction
+                                                     (:output nil)
+                                                     (:input t))
+                                                   *status-code-ptr*)
+                                      pointer)
+                           :direction direction))
+
+(defun read-dio-port (dio-port)
+  (unless (eq dio-port-direction :input)
+    (error "Not input direction dio-port."))
+  (getdio (dio-port-pointer pointer) *status-code-ptr*))
+
+(defun set-dio-port (dio-port new-value)
+  (unless (eq dio-port-direction :output)
+    (error "not output direction dio-port."))
+  (config-dio-port (dio-port-channel channel nil))
+  (setdio port (if new-value 1 0) *status-code-ptr*))
+>>>>>>> Unoobify code
 
 (defun clear-status ()
   (setf (mem-ref *status-code-ptr* :int32) 0))
@@ -41,28 +70,3 @@
      (let ((result (progn ,@code)))
        (print-status)
        result)))
-
-(defun get-dio-port (channel)
-  (if (or (< channel 0) (>= channel 26)) nullptr)
-  (when (null-pointer-p (aref *dio-ports* channel))
-      (setf (aref *dio-ports* channel)
-	    (check-status
-	      (initializedigitalport (getport channel) *status-code-ptr*)))
-      (check-status 
-	(allocatedio (aref *dio-ports* channel) t *status-code-ptr*)))
-  (aref *dio-ports* channel))
-
-(defun config-dio-port (channel input)
-  (let ((port (get-dio-port channel)))
-    (if (null-pointer-p port)
-	nil
-	(check-status
-	  (freedio port *status-code-ptr*)
-	  (allocatedio port input *status-code-ptr*)))))
-
-(defun set-dio-output (channel value)
-  (let ((port (get-dio-port channel)))
-    (if (null-pointer-p port) (return-from set-dio-output))
-    (config-dio-port channel nil)
-    (check-status
-      (setdio port (if value 1 0) *status-code-ptr*))))
